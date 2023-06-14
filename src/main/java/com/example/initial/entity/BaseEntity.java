66c03d95be13fa425ac9778303a53b581a6c8f2a
AppScAR <antoan@appscenic.com>
2023-06-14 16:15:21 +0300

    fix for tests

--- src/main/java/com/example/initial/entity/BaseEntity.java
+++ src/main/java/com/example/initial/entity/BaseEntity.java
@@ -2,9 +2,14 @@ package com.example.initial.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity {
 
