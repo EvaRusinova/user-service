35638a8620b4299525417075db3318ebb86e2bdf
AppScAR <antoan@appscenic.com>
2023-06-14 16:00:12 +0300

    to ativate spring data rest, only thing you need to do is add the dependency.

--- src/main/java/com/example/initial/controller/UserController.java
+++ src/main/java/com/example/initial/controller/UserController.java
@@ -4,38 +4,19 @@ import com.example.initial.dto.UserLoginDto;
 import com.example.initial.entity.User;
 import com.example.initial.interceptor.LoginCounterInterceptor;
 import com.example.initial.service.UserService;
-import java.util.List;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/user")
+@Tag(name = "Users")
+@RepositoryRestController
 @RequiredArgsConstructor
 public class UserController {
   private final UserService userService;
   private final LoginCounterInterceptor loginCounterInterceptor;
-
-  @PostMapping("/register")
-  public User registerUser(@RequestBody User user) {
-    return userService.registerUser(user);
-  }
-
-  @GetMapping("/{id}")
-  public User findById(@PathVariable(name = "id") Long id) {
-    return userService.findById(id);
-  }
-
-  @GetMapping("/{userName}")
-  public User findByUserName(@PathVariable(name = "userName") String userName) {
-    return userService.findByUserName(userName);
-  }
-
-  @GetMapping("/getAll")
-  public List<User> getAllUsers() {
-    return userService.getAllUsers();
-  }
 
   @PostMapping("/login")
   public ResponseEntity<String> login(@RequestBody UserLoginDto userLoginDto) {
