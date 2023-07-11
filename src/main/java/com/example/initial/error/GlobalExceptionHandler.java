e625f6dca9cac614c181e5d8546cd8bf045aa62b
AntoanRusinov <antoan.rusinov@gmail.com>
2023-07-11 16:02:39 +0300

    just updated one method

--- src/main/java/com/example/initial/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/initial/error/GlobalExceptionHandler.java
@@ -12,59 +12,57 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
-    return buildResponseEntity(HttpStatus.BAD_REQUEST, ex);
+    return buildErrorResponse(ex, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(DatabaseException.class)
   public ResponseEntity<ErrorResponse> handleDatabaseException(DatabaseException ex) {
-    return buildResponseEntity(HttpStatus.CONFLICT, ex);
+    return buildErrorResponse(ex, HttpStatus.CONFLICT);
   }
 
   @ExceptionHandler(AuthorizationException.class)
   public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException ex) {
-    return buildResponseEntity(HttpStatus.UNAUTHORIZED, ex);
+    return buildErrorResponse(ex, HttpStatus.UNAUTHORIZED);
   }
 
   @ExceptionHandler(ServerErrorException.class)
   public ResponseEntity<ErrorResponse> handleServerErrorException(ServerErrorException ex) {
-    return buildResponseEntity(HttpStatus.SERVICE_UNAVAILABLE, ex);
+    return buildErrorResponse(ex, HttpStatus.SERVICE_UNAVAILABLE);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<ErrorResponse> handleIllegalEx(IllegalArgumentException ex) {
-    return buildResponseEntity(HttpStatus.SERVICE_UNAVAILABLE, ex);
+    return buildErrorResponse(ex, HttpStatus.SERVICE_UNAVAILABLE);
   }
 
   @ExceptionHandler(DataIntegrityViolationException.class)
   public ResponseEntity<ErrorResponse> handleDataIntegrityViolationEx(
       DataIntegrityViolationException ex) {
-    return buildResponseEntity(HttpStatus.CONFLICT, ex);
+    return buildErrorResponse(ex, HttpStatus.CONFLICT);
   }
 
   @ExceptionHandler(InvalidCredentialsException.class)
   public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(
       InvalidCredentialsException ex) {
-    return buildResponseEntity(HttpStatus.UNAUTHORIZED, ex);
+    return buildErrorResponse(ex, HttpStatus.UNAUTHORIZED);
   }
 
-  @ExceptionHandler(RuntimeException.class)
-  public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
-    return buildResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, ex);
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+    return buildErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  private <T extends RuntimeException> ResponseEntity<ErrorResponse> buildResponseEntity(
-      HttpStatus status, T ex) {
-    String errorCode = null;
-    if (ex instanceof CustomException) {
-      errorCode = ((CustomException) ex).getErrorCode();
-    }
-
+  private ResponseEntity<ErrorResponse> buildErrorResponse(Exception ex, HttpStatus status) {
     ErrorResponse errorResponse =
         ErrorResponse.builder()
             .status(status.value())
             .message(ex.getMessage())
-            .errorCode(errorCode)
+            .errorCode(
+                ex instanceof CustomException
+                    ? ((CustomException) ex).getErrorCode()
+                    : "DEFAULT_ERROR_CODE")
             .build();
+
     return ResponseEntity.status(status).body(errorResponse);
   }
 }
